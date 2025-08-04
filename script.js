84046c849f6680605ad433ecf75751f3bad118f1
Rishikesh P C <rishikeshpc4545@gmail.com>
2025-08-04 23:47:21 +0530

    function for grid creatation created

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let evdvdsd =  ()=> {
+let container = document.querySelector('.container')
 
-}
 
-function hello() {
-    
+
+function gridCreate(rowNum,colNum) {
+    for (let i = 0; i < column; i++) {
+        let col = document.createElement('div')
+        col.classList.add('col')
+        for (let j = 0; j < row; j++) {
+            let ro = document.createElement('div')
+            ro.classList.add('ro')
+            ro.style.width = "20%"
+            col.appendChild(ro)
+        }
+        container.appendChild(col)
+    }
 }
