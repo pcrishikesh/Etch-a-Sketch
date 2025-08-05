dd672657a111d3416638536f66f8c5f44e762d18
Rishikesh P C <rishikeshpc4545@gmail.com>
2025-08-05 20:58:09 +0530

    when hover background color of each div's changes

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 let container = document.querySelector('.container')
 
+function main() {
+    gridCreate(16,16)
+    hoverChange()
+}
 
+main()
+
+// grid creating function
 
 function gridCreate(rowNum,colNum) {
-    for (let i = 0; i < column; i++) {
+    for (let i = 0; i < colNum; i++) {
         let col = document.createElement('div')
         col.classList.add('col')
-        for (let j = 0; j < row; j++) {
+        for (let j = 0; j < rowNum; j++) {
             let ro = document.createElement('div')
             ro.classList.add('ro')
+            ro.classList.add('hover')
             ro.style.width = "20%"
             col.appendChild(ro)
         }
         container.appendChild(col)
     }
+}
+
+// hover color change function
+
+function hoverChange() {
+    let divColorChange = document.querySelectorAll('.hover')
+
+    divColorChange.forEach((e)=> {
+        e.addEventListener('mouseover', (element)=> {
+            element.target.style.backgroundColor = "red"
+        })
+    })
+
 }
